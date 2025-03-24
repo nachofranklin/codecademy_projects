1cbd8d405ea64bf78a75e061e772ed6c6284b3d3
Nacho <nathanfranklin@live.co.uk>
2025-03-24 23:22:26 +0000

    add more graphs

--- Data Analyst/biodiversity_starter/biodiversity_starter.py
+++ Data Analyst/biodiversity_starter/biodiversity_starter.py
@@ -36,6 +36,7 @@ def print_value_counts(df):
 # Merging
 
 merged_df = pd.merge(obs_df, spec_df, on='scientific_name')
+merged_df['park_name'] = merged_df['park_name'].replace(' National Park', '', regex=True)
 # print(merged_df.head(10))
 
 # Variables
@@ -47,27 +48,57 @@ cons_status = spec_df['conservation_status'].unique()
 # What category of species has the most observations?
 
 top_species = merged_df.groupby('category')['observations'].sum().sort_values(ascending=False)
-sns.barplot(x=top_species.index, y=top_species.values, hue=top_species.index, palette='pastel')
-plt.xlabel('Species')
-plt.ylabel('Observations')
-plt.title('Total observations of different species\nacross all four National Parks')
-plt.xticks(rotation=30)
-plt.ticklabel_format(style='plain', axis='y')
-plt.show()
-plt.close()
+# sns.barplot(x=top_species.index, y=top_species.values, hue=top_species.index, palette='pastel')
+# plt.xlabel('Species')
+# plt.ylabel('Observations')
+# plt.title('Total observations of different species\nacross all four National Parks')
+# plt.xticks(rotation=30)
+# plt.ticklabel_format(style='plain', axis='y')
+# plt.show()
+# plt.close()
 
 # What park has the most endangered species? (! Not Threatened)
 # Species of Concern, Endangered, Threatened, In Recovery, Not Threatened
 
 conservation_df = merged_df[merged_df['conservation_status'] != 'Not Threatened']
 conservation = conservation_df.groupby('park_name')['observations'].sum().sort_values(ascending=False)
-sns.barplot(x=conservation.index, y=conservation.values, hue=conservation.index, palette='pastel')
-plt.xlabel('National Park')
-plt.ylabel('Observations')
-plt.title('Which National Park has the most sightings of any conservation species?')
-plt.xticks(rotation=30)
-plt.show()
-plt.close()
+# # sns.barplot(x=conservation.index, y=conservation.values, hue=conservation.index, palette='pastel')
+# # plt.xlabel('National Park')
+# # plt.ylabel('Observations')
+# # plt.title('Which National Park has the most sightings of any conservation species?')
+# # plt.xticks(rotation=30)
+# # plt.show()
+# # plt.close()
+# plt.pie(conservation, explode=(0.1, 0, 0, 0), autopct='%0.1f%%')
+# plt.title('Which Park has the most sightings of conservation species?')
+# plt.legend(title='National Park', labels=conservation.index)
+# plt.axis('equal')
+# plt.show()
+# plt.close()
+
+# What category has the most endangered species?
+
+endangered = conservation_df.groupby('category')['observations'].sum().sort_values(ascending=False)
+# sns.barplot(x=endangered.index, y=endangered.values, hue=endangered.index, palette='pastel')
+# plt.xlabel('Species')
+# plt.ylabel('Observations')
+# plt.title('Which category has the most sightings of any conservation species?')
+# plt.xticks(rotation=30)
+# plt.show()
+# plt.close()
+
+# What category has the highest percentage of endangered species?
+
+cons_percent = pd.merge(top_species, endangered, on='category')
+cons_percent['conservation_percentage'] = round(cons_percent['observations_y'] / cons_percent['observations_x'] * 100, 1)
+cons_percent = cons_percent.reset_index().rename(columns={'index': 'category'})
+# sns.barplot(data=cons_percent, x='category', y='conservation_percentage', hue='category', palette='pastel')
+# plt.xlabel('Species')
+# plt.ylabel('Percentage of species in conservation sightings / %')
+# plt.title('Percentage of observations which are conserved')
+# plt.xticks(rotation=30)
+# plt.show()
+# plt.close()
 
 
 # notes
@@ -75,3 +106,4 @@ plt.close()
 # there seems to be 5,824 species in each park and there is at least one observation of each species in each park
 # spec_df['conservation_status'] has 191 non null values, likely means all null values never gained conservation status and are okay
 # set all spec_df['conservation_status'] null values to = 'Not Threatened'
+# a lot of barplots, but not sure what else to investigate with the given data
